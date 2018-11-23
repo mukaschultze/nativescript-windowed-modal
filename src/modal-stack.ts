9611fefb50fb6f0546268932d708cfb5be27db94
Muka Schultze <samuelschultze@gmail.com>
2018-11-23 12:30:16 -0200

    ModalStack preventing event propagation on android

--- src/modal-stack.ts
+++ src/modal-stack.ts
@@ -1,4 +1,4 @@
-import { CSSType, View } from "tns-core-modules/ui/layouts/layout-base";
+import { CSSType, EventData, LayoutBase, View } from "tns-core-modules/ui/layouts/layout-base";
 import { StackLayout } from "tns-core-modules/ui/layouts/stack-layout/stack-layout";
 
 @CSSType("ModalStack")
@@ -11,23 +11,37 @@ export class ModalStack extends StackLayout {
     onLoaded(): void {
         super.onLoaded();
 
-        const modalView = this.getChildAt(0);
+        const modalView = <LayoutBase>this.getChildAt(0);
+
+        modalView.isPassThroughParentEnabled = false;
 
         this.set("height", "100%");
         this.set("width", "100%");
         this.horizontalAlignment = "center";
         this.verticalAlignment = "middle";
+        this.isPassThroughParentEnabled = false;
         this.addEventListener("tap", (evt) => this.outsideTap(evt, modalView));
 
     }
 
-    private outsideTap(args: any, modal: View): void {
+    private outsideTap(args: EventData, modal: View): void {
 
-        if (args.ios) {
-            const view = args.ios.view;
-            const tapPos = args.ios.locationInView(view);
+        const iosMotion = (<any>args).ios;
+        const androidMotion: android.view.MotionEvent = (<any>args).android;
+
+        if (iosMotion) {
+            const view = iosMotion.view;
+            const tapPos = iosMotion.locationInView(view);
             const modalFrame = modal.ios.frame;
             const insideRect = CGRectContainsPoint(modalFrame, tapPos);
+
+            if (insideRect) { // Touched inside, don't close.
+                return;
+            }
+        } else {
+            const rect = new android.graphics.Rect();
+            modal.android.getHitRect(rect);
+            const insideRect = rect.contains(androidMotion.getX(), androidMotion.getY());
 
             if (insideRect) { // Touched inside, don't close.
                 return;
